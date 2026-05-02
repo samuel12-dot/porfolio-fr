fde9f389e3ec802bdf83403bc6ed2f7119c99e43
samuel12-dot <samuelogunduyile76@gmail.com>
2026-05-02 23:50:25 +0100

    feat: update Header component for fixed positioning and add new Hero component
    - Changed Header component to use fixed positioning for better navigation.
    - Added new Hero component with a placeholder for the hero section.
    - Introduced several new SVG icons for improved UI.
    - Added multiple image assets for project showcase.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,11 @@
 export default function Header() {
     return (
-        <div className="flex justify-center items-center relative top-3">
+        <div className="flex justify-center items-center fixed top-3 w-full">
             <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/15 backdrop-blur">
                 <a href="#" className="nav-item">Home</a>
                 <a href="#project" className="nav-item">Project</a>
                 <a href="#about" className="nav-item">About</a>
-                <a href="#projects" className="nav-item bg-white text-gray-900 rounded-full hover:bg-white/70 hover:text-gray-900">Contact</a>
+                <a href="#projects" className="nav-item bg-white text-gray-900 rounded-full hover:bg-white/70 hover:">Contact</a>
             </nav>
         </div>
     )
